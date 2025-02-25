06fbf7b5deb12dfc23acf9d7b6c03d9afdc590b7
Arjun <arjunalukkal@gmail.com>
2025-02-24 19:37:35 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Arjun Alukkal on 2025-02-24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
